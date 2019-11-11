76ea1be5a0534b041826cce401b4f8d89de32d3b
ArtemPC <mosolov.uspeh@gmail.com>
2019-11-11 11:48:22 +0300

    2222

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 
-    <div class="container 3">
+    <div class="container">
         <div class="row">
             <div class="col-md-12 justify-content-center">
                 <?php the_posts_pagination([
